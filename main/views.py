3812340a761a80c87df5bd742d7381d2deaae71e
Zhalil Omuraliev <yorneo@bk.ru>
2021-01-25 14:20:11 +0600

    added todo closing

--- main/views.py
+++ main/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, HttpResponse
-from .models import TODO
+from .models import todo
 
 def homepage(request):
     return render(request, "index.html")
 
 def test(request):
-    todo_list = TODO.objects.all()
+    todo_list = ToDo.objects.all()
     return render(request, "test.html")
 
 def second(request):
@@ -17,17 +17,23 @@ def third(request):
 def add_todo(request):
     form = request.POST
     text = form["todo_text"]
-    todo = TODO(text=text)
+    todo = ToDo(text=text)
     todo.save()
     return HttpResponse("Форма получена")
 
 def delete_todo(request, id):
-    todo = TODO.objects.get(id=id)
+    todo = ToDo.objects.get(id=id)
     todo.delete()
     return redirect(test)
 
 def mark_todo(request, id):
-    todo  TODO.objects.get(id=id)
+    todo  ToDo.objects.get(id=id)
     todo.is_favorite = True
+    todo.save()
+    return redirect(test)
+
+def close_todo(request, id):
+    todo = ToDo.objects.get(id=id)
+    todo.is_closed = not todo.is_closed
     todo.save()
     return redirect(test)
