10de4c25800a26596563aa58532b230ed7565209
Zhalil Omuraliev <yorneo@bk.ru>
2021-01-28 13:38:50 +0600

    added homework books and homework details

--- main/models.py
+++ main/models.py
@@ -8,3 +8,14 @@ class ToDo(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     is_closed = models.BooleanField(default=False)
     is_favorite= models.BooleanField(default=False)
+
+class Books(models.Model):
+    title = models.CharField(max_length=50)
+    subtitle = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    price = models.CharField(max_length=5)
+    genre = models.CharField(max_length=50)
+    author = models.CharField(max_length=50)
+    year = models.CharField(max_length=4)
+    date = models.DateTimeField(auto_now_add=True)
+    is_favorites = models.BooleanField(default=False)
--- main/views.py
+++ main/views.py
@@ -37,3 +37,41 @@ def close_todo(request, id):
     todo.is_closed = not todo.is_closed
     todo.save()
     return redirect(test)
+
+def books(request):
+    books_list = Books.objects.all()
+    return render(request, "books.html", {"books_list": books_list})
+
+def add_book(request):
+    form = request.POST
+    title = form["book_title"]
+    subtitle = form["book_subtitle"]
+    author = form["book_author"]
+    price = form["book_price"]
+    genre = form["book_genre"]
+    description = form["book_description"]
+    year = form["book_year"]
+    b = Books(title=title, subtitle = subtitle, author=author, price=price, genre=genre, description=description, year=year)
+    b.save()
+    return redirect(books)
+
+def delete_book(request, id):
+    b = Books.objects.get(id=id)
+    b.delete()
+    return redirect(books)
+
+def mark_book(request, id):
+    b = Books.objects.get(id=id)
+    b.is_favorites = True
+    b.save()
+    return redirect(books)
+
+def unmark_book(request, id):
+    b = Books.objects.get(id=id)
+    b.is_favorites = False
+    b.save()
+    return redirect(books)
+
+def BooksDetail(request, id):
+    book_object = Books.objects.get(id=id)
+    return render(request, "book_detail.html", {"book_object": book_object})
